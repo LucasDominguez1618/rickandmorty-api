95d7da7e79cb5087f88f3b7e5668bf2e93e750dd
Lucas Dominguez <lucasdomin62@gmail.com>
2023-10-04 15:05:23 -0300

    titulo hecho

--- rickandmorty-api/src/components/character/Character.jsx
+++ rickandmorty-api/src/components/character/Character.jsx
@@ -3,19 +3,32 @@ import "./character.css";
 export function Character() {
   //useState
   const [character, setCharacter] = useState([]);
-
-  //llaves
-  //useEffect y useState Hooks
+  const primerCapitulo = [];
+  character.map((item) => {
+    primerCapitulo.push(item.episode[0]);
+  });
+  console.log(primerCapitulo);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
       .then((response) => response.json())
       .then((data) => setCharacter(data.results));
   }, [setCharacter]);
+  //llaves
+  //useEffect y useState Hooks
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${primerCapitulo}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   //useEffect
   return (
     <>
+      <div className="Fondo">
+        <h1 className="titulo"> The Rick and Morty API </h1>
+      </div>
       <div className="fondo2">
         <ul className="Personajes">
           {character.map((item, index) => (
@@ -25,10 +38,17 @@ export function Character() {
 
                 <div className="ch">
                   <h3 className="name">{item.name}</h3>
-                  <p className="doa">
-                    {" "}
-                    {item.status} - {item.species}
-                  </p>{" "}
+                  {item.status === "Alive" ? (
+                    <p className="doa">
+                      {" "}
+                      🟢 {item.status} - {item.species}
+                    </p>
+                  ) : (
+                    <p className="doa">
+                      {" "}
+                      🔴 {item.status} - {item.species}{" "}
+                    </p>
+                  )}
                   <p className="ty"> {item.type}</p>
                   <p className="lkl">Last known location:</p>
                   <p className="or"> {item.origin.name}</p>
